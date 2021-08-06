474cf1d436eb58d2bc6866d82acc35d3328b85e1
Cody Hill <cody.hill07@gmail.com>
2021-08-06 01:15:54 -0500

    good?

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,4 +2,16 @@ class PlayersController < ApplicationController
   def index
     @players = Player.where(team_id: nil)
   end
+
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  def update
+    @player = Player.find(params[:id])
+
+    @player.update(team_id: params[:player][:team_id])
+
+    redirect_to root_path
+  end
 end
